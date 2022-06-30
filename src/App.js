1787601ab3833840d6095d7def89db035609916e
chandanil <you@example.com>
2022-06-30 16:02:41 +0545

    Line chart added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "../src/Components/Style/All.scss";
-import "../src/Components/Style/Variables.scss";
-import "../src/Components/Style/Mixin.scss";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./Components/Header/Header";
 import { Navbar, Navs } from "./Components/Navbar/Navbar";
@@ -9,6 +8,8 @@ import { InfoCard } from "./Components/InfoCard/InfoCard";
 import { Table } from "./Components/Table/Table";
 import { LineChart } from "./Components/LineChart/LineChart";
 import { Form } from "./Components/Form/Form";
+import { useState } from "react";
+
 
 const App = () => {
   return (
--- src/Components/LineChart/LineChart.jsx
+++ src/Components/LineChart/LineChart.jsx
@@ -1,31 +1,119 @@
-// import Chart from "react-apexcharts";
-import { Todo } from "../ToDo/Todo";
 
+import "./LineChart.scss";
+import ReactApexChart from "react-apexcharts";
+import { Todo } from "../ToDo/Todo";
 export const LineChart = () => {
+  const series = [
+    {
+      name: "REVENUE",
+      data: [52, 61, 64, 15, 28, 77, 44, 5, 29, 35, 56, 67, 33, 33],
+    },
+    {
+      name: "EXPENSESS",
+      data: [12, 41, 4, 38, 28, 24, 22, 45, 29, 23, 12, 30, 32, 43],
+    },
+  ];
+  const options: ApexOptions = {
+    chart: {
+      type: "line",
+      toolbar: {
+        show: true,
+      },
+    },
+    plotOptions: {
+      bar: {
+        distributed: true,
+      },
+    },
+    colors: ["#405189", "#f06548", "#E35F55"],
+    dataLabels: {
+      enabled: false,
+    },
+
+    stroke: {
+      width: 3,
+      curve: "smooth",
+    },
+
+    yaxis: {
+      min: 0,
+    },
+    xaxis: {
+      labels: {
+        style: {
+          colors: "#000"
+        }
+      },
+      title: {
+        text: "REVENUE",
+        style: {
+          color: "#405189"
+        }
+      }
+    },
+
+    legend: {
+      position: "top",
+      horizontalAlign: "right",
+      floating: true,
+      offsetY: -25,
+      offsetX: -5,
+    },
+  };
   return (
-    <>
-      <div className="body-section">
-        <div className="container-fluid">
-          <div className="row">
-            <div class="col-md-8">
-              <div className="linechart">
-                <div className="cl-header">
-                  <h5 className="title">Balance Overview</h5>
+    <div className="body-section">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8">
+            <div className="cl-chart">
+              <div className="cl-header">
+                <h2 className="title">Balance Overview</h2>
+                <div className="cl-filter">
+                  <span>Sort By :</span>
+                  <select>
+                    <option>Current Year</option>
+                    <option>Past Year</option>
+                    <option>2013</option>
+                    <option>Current Year</option>
+                  </select>
                 </div>
-                {/* <Chart
-    options={this.state.options}
-    series={this.state.series}
-    type="line"
-    width="500"
-  /> */}
+              </div>
+              <div className="line-chart-section">
+                <div className="bar-title">
+                  <div className="d-end">
+                    <div className="filter-block">
+                      {/* <DropDown
+                        theme="filter"
+                        optionsProps={[
+                          {
+                            value: "This month",
+                            label: "This month",
+                          },
+                          {
+                            value: "Last 40 days",
+                            label: "Last 40 days",
+                          },
+                        ]}
+                      /> */}
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <ReactApexChart
+                    series={series}
+                    options={options}
+                    type="line"
+                    height={350}
+                  />
+                </div>
               </div>
             </div>
-            <div className="col-md-4">
-              <Todo />
-            </div>
+          </div>
+          <div className="col-md-4">
+            <Todo/>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
